598763c6cc723cd898b3e71c7fe418ceb02e2af8
Artjoms Vorona <artjomsvorona@gmail.com>
2019-11-22 17:10:39 +0200

    Segmented Control allows user to decide if he would like to save counter in UserDefaults

--- Persist Data Example/Controllers/ViewController.swift
+++ Persist Data Example/Controllers/ViewController.swift
@@ -11,20 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     var counter = 0
-    var shouldSaveCounter = true
     let userDefaults = UserDefaults.standard
     
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var shouldSaveCounterSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var shouldSaveSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        if let shouldSave = userDefaults.object(forKey: "shouldSaveCounter") {
+            if shouldSave as! Bool {
+                shouldSaveSegmentedControl.selectedSegmentIndex = 0
+            } else {
+                shouldSaveSegmentedControl.selectedSegmentIndex = 1
+                userDefaults.set(0, forKey: "counter")
+            }
+        }
+        
         counter = userDefaults.integer(forKey: "counter")
         counterLabel.text = String(counter)
     }
     
     @IBAction func saveCounterControlValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            userDefaults.set(true, forKey: "shouldSaveCounter")
+        } else {
+            userDefaults.set(false, forKey: "shouldSaveCounter")
+        }
     }
     
     @IBAction func addOneButtonTapped(_ sender: UIButton) {
